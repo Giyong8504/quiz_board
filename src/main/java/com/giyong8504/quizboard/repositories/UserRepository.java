eac7f0b42259b485f55ef966be84d03958679ae5
Giyong8504 <kky5163@naver.com>
2024-12-22 13:51:33 +0900

    Querydls 의존성 추가 및 저장소에 email있는 지 확인 작성

--- src/main/java/com/giyong8504/quizboard/repositories/UserRepository.java
+++ src/main/java/com/giyong8504/quizboard/repositories/UserRepository.java
@@ -1,11 +1,18 @@
 package com.giyong8504.quizboard.repositories;
 
+import com.giyong8504.quizboard.entities.QUser;
 import com.giyong8504.quizboard.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, QuerydslPredicateExecutor {
 
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(String email); // 사용자 email 조회
+
+    default boolean exists(String email) { // 이메일이 존재하는지 확인.
+        return exists(QUser.user.email.eq(email)); // Querydls을 사용하여 싱글톤 패턴으로 항목 조회
+    }
+
 }
